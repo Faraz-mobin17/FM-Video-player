46ba7b613f26bd211f199f3fb6367438f3a7a4f8
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-12-30 00:15:29 +0530

    made search bar working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,28 +2,37 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
-const SearchBar = () => (
-  <Paper
-    component="form"
-    onSubmit={() => {}}
-    sx={{
-      borderRadius: 20,
-      border: "1px solid #e3e3e3",
-      pl: 2,
-      boxShadow: "none",
-      mr: { sm: 5 },
-    }}
-  >
-    <input
-      className="search-bar"
-      placeholder="Search..."
-      value=""
-      onChange={() => {}}
-    />
-    <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
-      <Search />
-    </IconButton>
-  </Paper>
-);
+
+const SearchBar = () => {
+  const [searchText, setSearchText] = useState("");
+  return (
+    <Paper
+      component="form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        // setSearchText(e.target.value);
+      }}
+      sx={{
+        borderRadius: 20,
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+        style={{ border: "none", outline: "none" }}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
 
 export default SearchBar;
